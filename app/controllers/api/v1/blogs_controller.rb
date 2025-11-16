ba049410882070e7886ca866e60b94e8ccd13749
yusuke <yske.biz@gmail.com>
2025-11-17 00:41:56 +0900

    feat: add CRUD method

--- app/controllers/api/v1/blogs_controller.rb
+++ app/controllers/api/v1/blogs_controller.rb
@@ -1,22 +1,46 @@
 class Api::V1::BlogsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def index
-    @index= "hello index"
+    @blogs = Blog.all
 
-    render json: { message: @index }
+    render json: @blogs
   end
 
   def create
+      @blog = Blog.new(blog_params)
+
+      if @blog.save
+        render json: @blog, status: :created
+      else
+        render json: @blog.errors, status: :unprocessable_entity
+      end
   end
 
   def show
-    @show= "hello show"
+    @blog = Blog.find(params[:id])
 
-    render json: { message: @show }
+    render json: @blog
   end
 
   def update
+    @blog = Blog.find(params[:id])
+
+    if @blog.update(blog_params)
+      render json: @blog
+    else
+      render json: @blog.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @blog = Blog.find(params[:id])
+    @blog.destroy
+
+    head :no_content
+  end
+
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content)
   end
 end
